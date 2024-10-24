7533b410424821528538c9acad28490713540139
Michael Nobles <Mnobles33@gmail.com>
2024-10-24 13:04:46 -0700

    First Commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import { NavLink, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div id="app">
       <nav>
-        {/* NavLinks here */}
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/order'>Order</NavLink>
       </nav>
-      {/* Route and Routes here */}
-      <Home />
-      <Form />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/order' element={<Form />} />
+      </Routes>
     </div>
   )
 }
